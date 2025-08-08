1aa763ad8fd4db83d5a131f151c0358f3a89265d
nuel232 <nwankwoala3@gmail.com>
2025-08-08 23:54:41 +0100

    feat: completed the UI(frontend) of the login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,17 @@
+import 'package:authentication/components/my_button.dart';
 import 'package:authentication/components/my_textfield.dart';
+import 'package:authentication/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
   //text editing controller
   final usernameController = TextEditingController();
+
   final passwordController = TextEditingController();
+
+  void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +21,8 @@ class _LoginPageState extends State<LoginPage> {
       body: SafeArea(
         child: Center(
           child: Column(
+            //this make it easier when working with different scree sizes
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 50),
               //logo
@@ -55,25 +57,87 @@ class _LoginPageState extends State<LoginPage> {
               SizedBox(height: 15),
 
               //forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  //since we want this to be at the right side but the column is center we wrap the text widget into Row
+                  //and set the mainAxisAlignment to end
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 25),
+
+              // sign in button
+              MyButton(onTap: signUserIn),
+
+              SizedBox(height: 50),
+
+              //or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+
+                    Expanded(
+                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 50),
+
+              // goggle + apple sign in buttons
               Row(
-                //since we want this to be at the right side but the column is center we wrap the text widget into Row
-                //and set the mainAxisAlignment to end
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Forgot Password?',
-                    style: TextStyle(color: Colors.grey[600]),
-                  ),
+                  //google button
+                  SquareTile(imagePath: 'lib/images/Google-logo.png'),
+
+                  SizedBox(width: 25),
+                  //apple button
+                  SquareTile(imagePath: 'lib/images/apple-logo.png'),
                 ],
               ),
 
-              // sign in button
-
-              //or continue with
-
-              // goggle + apple sign in buttons
+              SizedBox(height: 30),
 
               //not a member? register
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
