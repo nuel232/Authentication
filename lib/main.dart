0ccafd602022d5dd142970ddc71d0bfd07850e54
nuel232 <nwankwoala3@gmail.com>
2025-08-12 23:14:04 +0100

    feat: add Google Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:authentication/pages/auth_pages.dart';
-import 'package:authentication/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
--- lib/pages/auth_pages.dart
+++ lib/pages/auth_pages.dart
@@ -1,6 +1,5 @@
 import 'package:authentication/pages/home_page.dart';
 import 'package:authentication/pages/login_or_register_page.dart';
-import 'package:authentication/pages/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
